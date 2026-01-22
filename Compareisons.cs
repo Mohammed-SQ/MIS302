4e8a3856cdedc55b49fecba1143c7d14c1133331
ALLUQMAN, MUHAMMED SUFYAN MUHAMMED <147169870+Mohammed-SQ@users.noreply.github.com>
2026-01-22 09:15:54 +0000

    update compare file

--- Compareisons.cs
+++ Compareisons.cs
@@ -9,15 +9,48 @@ class Compareisons
         int secondNumber = int.Parse(Console.ReadLine());
         Console.Write("Enter third number: ");
         int thirdNumber = int.Parse(Console.ReadLine());
+        if (firstNumber == secondNumber)
+        {
+            if (secondNumber == thirdNumber)
+            {
+                Console.WriteLine("All numbers are equal");
+            }
+        }
+        if (firstNumber != secondNumber)
+        {
+            if (firstNumber != thirdNumber)
+            {
+                if (secondNumber != thirdNumber)
+                {
+                    Console.WriteLine("All numbers are different");
+                }
+            }
+        }
         int largest = firstNumber;
-        if (secondNumber > largest)
+        if (largest < secondNumber)
         {
             largest = secondNumber;
         }
-        if (thirdNumber > largest)
+
+        if (largest <= thirdNumber)
         {
             largest = thirdNumber;
         }
-        Console.WriteLine("The largest number is: " + largest);
+
+        Console.WriteLine("Largest number: " + largest);
+
+        int smallest = firstNumber;
+        if (smallest > secondNumber)
+        {
+            smallest = secondNumber;
+        }
+
+        if (smallest >= thirdNumber)
+        {
+            smallest = thirdNumber;
+        }
+        
+        Console.WriteLine("Smallest number: " + smallest);
     }
 }
+    
